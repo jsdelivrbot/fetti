564de90f4d0f3b047eb074391c98767bf55f9504
stvyzxx <yurasobechko@gmail.com>
2018-06-15 02:02:08 +0300

    Added new packages for scrapper

---
+++ core/scrapper.js
@@ -0,0 +1,10 @@
+const scrapper = {
+  name: 'fetti',
+  activate: activate
+};
+
+function activate() {
+  console.log('activated');
+}
+
+module.exports = scrapper;
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
+const scrapper = require('./core/scrapper');
+
+scrapper.activate();
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -8,3 +11,4 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
